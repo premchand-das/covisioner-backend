25be7607be72bfae881bd06504d924bb800cc970
premchand-das <premd7745@gmail.com>
2026-06-20 21:38:20 +0530

    Use Gmail SMTP host config

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,24 +2,19 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-    console.log("EMAIL_USER EXISTS:", Boolean(process.env.EMAIL_USER));
-    console.log("EMAIL_PASS EXISTS:", Boolean(process.env.EMAIL_PASS));
-    console.log("SENDING EMAIL TO:", to);
-
-    if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-      throw new Error("Missing EMAIL_USER or EMAIL_PASS env variables");
-    }
-
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      connectionTimeout: 30000,
+      greetingTimeout: 30000,
+      socketTimeout: 30000,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
     });
-
-    await transporter.verify();
-    console.log("SMTP VERIFIED");
 
     const info = await transporter.sendMail({
       from: `"CoVisioner Platform" <${process.env.EMAIL_USER}>`,
@@ -29,10 +24,9 @@ export const sendEmail = async ({ to, subject, html }) => {
     });
 
     console.log("EMAIL SENT:", info.messageId);
-
     return info;
   } catch (error) {
-    console.error("EMAIL ERROR:", error);
+    console.error("EMAIL SEND ERROR:", error);
     throw error;
   }
 };
